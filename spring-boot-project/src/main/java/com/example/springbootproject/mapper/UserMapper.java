3a8f2e0ef8c20507a4857e106b3bd2259b5e2e0b
linzhou <970060731@qq.com>
2022-03-10 21:25:29 +0800

    init

--- spring-boot-project/src/main/java/com/example/springbootproject/mapper/UserMapper.java
+++ spring-boot-project/src/main/java/com/example/springbootproject/mapper/UserMapper.java
@@ -1,18 +1,59 @@
 package com.example.springbootproject.mapper;
 
+import com.example.springbootproject.entity.UserEntity;import org.apache.ibatis.annotations.Param;import java.util.List;
 
-import com.example.springbootproject.entity.User;
-
+/**
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //         佛祖保佑           永无BUG           永不修改              //
+ * //          佛曰:                                                  //
+ * //                 写字楼里写字间，写字间里程序员;                      //
+ * //                 程序人员写程序，又拿程序换酒钱.                      //
+ * //                 酒醒只在网上坐，酒醉还来网下眠;                      //
+ * //                 酒醉酒醒日复日，网上网下年复年.                      //
+ * //                 但愿老死电脑间，不愿鞠躬老板前;                      //
+ * //                 奔驰宝马贵者趣，公交自行程序员.                      //
+ * //                 别人笑我忒疯癫，我笑自己命太贱;                      //
+ * //                 不见满街漂亮妹，哪个归得程序员?                      //
+ * ////////////////////////////////////////////////////////////////////
+ *
+ * @date : 2022/3/10 20:52
+ * @author: linzhou
+ * @description : UserMapper
+ */
 public interface UserMapper {
     int deleteByPrimaryKey(Long id);
 
-    int insert(User record);
+    int insert(UserEntity record);
 
-    int insertSelective(User record);
+    int insertSelective(UserEntity record);
 
-    User selectByPrimaryKey(Long id);
+    UserEntity selectByPrimaryKey(Long id);
 
-    int updateByPrimaryKeySelective(User record);
+    int updateByPrimaryKeySelective(UserEntity record);
 
-    int updateByPrimaryKey(User record);
+    int updateByPrimaryKey(UserEntity record);
+
+    List<UserEntity> selectByName1(@Param("name") String name);
+
+
+
 }
