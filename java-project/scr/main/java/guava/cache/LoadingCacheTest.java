3ab3d36c20993da45b9c1e5d543ca68714585a13
linzhou <linzhou@duiba.com.cn>
2022-03-17 15:24:18 +0800

    init

--- java-project/scr/main/java/guava/cache/LoadingCacheTest.java
+++ java-project/scr/main/java/guava/cache/LoadingCacheTest.java
@@ -17,7 +17,8 @@ public class LoadingCacheTest {
 
     private static LoadingCache<String, String> graphs = CacheBuilder.newBuilder()
             .maximumSize(1000)
-            .expireAfterWrite(1, TimeUnit.SECONDS)
+            .expireAfterWrite(3, TimeUnit.SECONDS)
+            .expireAfterAccess(1,TimeUnit.SECONDS)
             .build(
                     new CacheLoader<String, String>() {
                         @Override
@@ -30,52 +31,25 @@ public class LoadingCacheTest {
         graphs.put("key1","key1Value");
         graphs.put("key2","key2Value");
 
-        try {
-            System.out.println(graphs.get("key1"));
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        try {
-            System.out.println(graphs.get("key2"));
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+        for (int i = 0; i < 100; i++) {
+            try {
+                System.out.println(graphs.get("key1"));
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            try {
+                System.out.println(graphs.get("key2"));
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
 
-        try {
-            Thread.sleep(500L);
-            System.out.println("=========500ms===============");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(500L);
+                System.out.println(i+"=========500ms===============");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        try {
-            System.out.println(graphs.get("key1"));
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        try {
-            System.out.println(graphs.get("key2"));
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Thread.sleep(500L);
-            System.out.println("===============1000============");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            System.out.println(graphs.get("key1"));
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-        try {
-            System.out.println(graphs.get("key2"));
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-
 
     }
 
